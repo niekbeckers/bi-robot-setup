c9fca16e0665195694fd90fb774527c9786399a2
Niek <niekbeckers@gmail.com>
2017-02-23 11:35:04 +0100

    moved requested experiment state (global) inside robot block, changed colours

--- simulink/experiments/base_1fm_parameters.m
+++ simulink/experiments/base_1fm_parameters.m
@@ -9,6 +9,8 @@ Lb = 0.07;          % base width [m]
 %% RobotStruct_FM1
 clear FM1
 
+FM1.HomeLocationOpSpace = [0;0.25];                     % home location (homing) [m]
+
 % actuator 1 data
 FM1.Actuator1.JointAbsoluteEncoderCounts_rev = 2^16;    % encoder counts per revolution [counts]
 FM1.Actuator1.AbsEncoderOffset = 0;                     % offset absolute encoder [rad]
@@ -21,6 +23,19 @@ FM1.Actuator1.AbsoluteMaximumTorque_Nm = 8;             % maximum allowable torq
 FM1.Actuator1.TransmissionSlipAngleGuardMax = 5*pi/180; % maximum allowable transmission slip [Nm]
 FM1.Actuator1.JointPositionGuardMin = -25*pi/180;       % maximum allowable joint position [rad]
 FM1.Actuator1.JointPositionGuardMax = 135*pi/180;       % maximum allowable joint position [rad]
+
+% actuator 2 data
+FM1.Actuator2.JointAbsoluteEncoderCounts_rev = 2^16;    % encoder counts per revolution [counts]
+FM1.Actuator2.AbsEncoderOffset = 0;                     % offset absolute encoder [rad]
+FM1.Actuator2.MotorEncoderCounts_rev = 2^12;            % motor encoder resolution [counts] (1024 lines & X4: 4096 counts)
+FM1.Actuator2.FOAWNoiseLevel = 2*pi/FM1.Actuator1.JointAbsoluteEncoderCounts_rev; % Noise level for FOAW algorithm
+FM1.Actuator2.transmissionRatio = 7.3;                  % transmission ratio [-]
+FM1.Actuator2.motorTorqueConstant = 84.86*1e-3;         % motor torque constant [Nm/A]
+FM1.Actuator2.drivePeakCurrent = 20;                    % drive peak current [A]
+FM1.Actuator2.AbsoluteMaximumTorque_Nm = 8;             % maximum allowable torque (at joints) [Nm]
+FM1.Actuator2.TransmissionSlipAngleGuardMax = 5*pi/180; % maximum allowable transmission slip [Nm]
+FM1.Actuator2.JointPositionGuardMin = -25*pi/180;       % maximum allowable joint position [rad]
+FM1.Actuator2.JointPositionGuardMax = 135*pi/180;       % maximum allowable joint position [rad]
 
 
 
