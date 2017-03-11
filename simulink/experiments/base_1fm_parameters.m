69fb1774b85d92ae393a184bcd54732097d01664
Niek <niekbeckers@gmail.com>
2017-03-11 17:48:28 +0100

    added force sensor transformation matrix as function handle in struct

--- simulink/experiments/base_1fm_parameters.m
+++ simulink/experiments/base_1fm_parameters.m
@@ -42,3 +42,17 @@ FM1.Actuator2.JointPositionGuardMax = 205*pi/180;       % maximum allowable join
 
 % force/torque sensor data
 FM1.FTSensor.MaxAllowableForcesTorqueSensor = [100 100 200 2 2 2]*0.7; % maximum allowable forces and torques
+
+% transformation matrices
+syms alpha q1 q2 q3 q4 q5
+alpha = pi - q2; % if the sensor is attached to the other side: use q5 and alpha = -(pi-q4)
+R1 = [cos(q1) -sin(q1) 0; sin(q1) cos(q1) 0; 0 0 1];
+R2 = [cos(alpha) -sin(alpha) 0; sin(alpha) cos(alpha) 0; 0 0 1];
+R3 = [cos(-0.5*pi) -sin(-0.5*pi) 0;sin(-0.5*pi) cos(0.5*pi) 0; 0 0 1];
+R = inv(R3*R2*R1);
+R = blkdiag(R,R); % transform forces and moments
+FM1.FTSensor.Tmatrix_fcn = matlabFunction(R,'Vars',{[q1;q2;q3;q4;q5]});
+clear R R1 R2 R3 q1 q2 q3 q4 q5 alpha
+
+
+
