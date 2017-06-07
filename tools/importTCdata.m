93de5e4b75ce6ab86773b87c06dfa58127585a9b
Niek-TwinCAT <niekbeckers@gmail.com>
2017-06-07 11:02:50 +0200

    Worked a lot on the velocity estimation. Many changes. See issues in bitbucket for more info.

--- tools/importTCdata.m
+++ tools/importTCdata.m
@@ -21,7 +21,7 @@ currentdir = pwd;
 
 cd(datapath); % go to data directory
 
-datafiles = dir([filename_str '_part*.mat']);   % get list of all datafiles
+datafiles = dir('data_part*.mat');   % get list of all datafiles
 filenames = sort_nat({datafiles(:).name});      % sort files (sort_nat needed)
 
 dataArray = [];
@@ -39,25 +39,24 @@ dataArray = dataArray';       % to columns
 
 % load and prepare variable names
 load_system(get_param(modelname,'filename'));
-str_param_BROS1 = strrep(strsplit(get_param([modelname '/BusSelectorLoggerBROS1'],'OutputSignals'),','),'.','_').';
+str_param_BROS1 = strsplit(get_param([modelname '/BusSelectorLoggerBROS1'],'OutputSignals'),',').';
 str_param_BROS1 = strcat(str_param_BROS1,'_BROS1');
-str_param_BROS2 = strrep(strsplit(get_param([modelname '/BusSelectorLoggerBROS1'],'OutputSignals'),','),'.','_').';
+str_param_BROS2 = strsplit(get_param([modelname '/BusSelectorLoggerBROS2'],'OutputSignals'),',').';
 str_param_BROS2 = strcat(str_param_BROS2,'_BROS2');
 
 % these are the parameter labels in the recorded data
 param_lbls = ['time';str_param_BROS1;str_param_BROS2;'target_BROS1';'target_BROS2';'cursor_BROS1';'cursor_BROS2';'Error_BROS1';'Error_BROS2';'ExpTrialNumber';'ExpTrialRunning'];   
+
 % indices corresponding to each parameter
 param_idx = {1; 2; 3:4; 5:6; 7:8; 9:10; 11:12; 13:14; 15:16; 17:18; 19:20; 21:22; 23:28; 29; 30;...
                31; 32:33; 34:35; 36:37; 38:39; 40:41; 42:43; 44:45; 46:47; 48:49; 50:51; 52:57; 58; 59;...
                57:58; 59:60; 61:62; 63:64; 65; 66; 67; 68};
 
-          
 % create struct with all data parameters
 data = struct;
 for ii = 1:length(param_lbls)
     param = param_lbls{ii};
-    if ~isempty(strfind(param,'.'))
-        param = extractAfter(param,'.');
-    end
-    data.(param) = dataArray(:,param_idx{ii});
+    if ~isempty(strfind(param,'.')), param = extractAfter(param,'.'); end
+    
+    eval(['data.' char(param) ' = dataArray(:,param_idx{ii});']);
 end
