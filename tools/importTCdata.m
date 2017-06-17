091a6037c9d367eace1c44c92ebf4182bdfe8fbd
Niek <niekbeckers@gmail.com>
2017-06-17 16:17:09 +0200

    Updated script, condotional model needed to load parameters

--- tools/importTCdata.m
+++ tools/importTCdata.m
@@ -1,4 +1,3 @@
-
 function data = importTCdata(datapath, modelname)
 %% importTCdata
 %
@@ -9,19 +8,29 @@ function data = importTCdata(datapath, modelname)
 % Niek Beckers
 
 %% check input
-
 % no data path given? Call UI to select folder manually
 if isempty(datapath)
     datapath = uigetdir;
+end
+if nargin == 2
+    % model name is given, load variable names from model
+    load_system(get_param(modelname,'filename'));
+    str_param_BROS1 = strsplit(get_param([modelname '/BusSelectorLoggerBROS1'],'OutputSignals'),',').';
+    str_param_BROS1 = strcat(str_param_BROS1,'_BROS1');
+    str_param_BROS2 = strsplit(get_param([modelname '/BusSelectorLoggerBROS2'],'OutputSignals'),',').';
+    str_param_BROS2 = strcat(str_param_BROS2,'_BROS2');
+else
+    str_param_BROS1 = '';
+    str_param_BROS2 = '';
 end
 
 %% load measurement data
 % currentdir = [fileparts(mfilename('fullpath')) filesep];
 currentdir = pwd;
 
-cd(datapath); % go to data directory
+cd(datapath);                                   % go to data directory
 
-datafiles = dir('data_part*.mat');   % get list of all datafiles
+datafiles = dir('data_part*.mat');              % get list of all datafiles
 filenames = sort_nat({datafiles(:).name});      % sort files (sort_nat needed)
 
 dataArray = [];
@@ -34,18 +43,21 @@ for ii = 1:length(filenames)
     clear data_*
 end
 
-cd(currentdir);
+cd(currentdir); % go back to current directory
 dataArray = dataArray';       % to columns
 
-% load and prepare variable names
-load_system(get_param(modelname,'filename'));
-str_param_BROS1 = strsplit(get_param([modelname '/BusSelectorLoggerBROS1'],'OutputSignals'),',').';
-str_param_BROS1 = strcat(str_param_BROS1,'_BROS1');
-str_param_BROS2 = strsplit(get_param([modelname '/BusSelectorLoggerBROS2'],'OutputSignals'),',').';
-str_param_BROS2 = strcat(str_param_BROS2,'_BROS2');
-
 % these are the parameter labels in the recorded data
-param_lbls = ['time';str_param_BROS1;str_param_BROS2;'target_BROS1';'target_BROS2';'cursor_BROS1';'cursor_BROS2';'Error_BROS1';'Error_BROS2';'ExpTrialNumber';'ExpTrialRunning'];   
+param_lbls = ['time';
+    str_param_BROS1;
+    str_param_BROS2;
+    'target_BROS1';
+    'target_BROS2';
+    'cursor_BROS1';
+    'cursor_BROS2';
+    'Error_BROS1';
+    'Error_BROS2';
+    'ExpTrialNumber';
+    'ExpTrialRunning'];   
 
 % indices corresponding to each parameter
 param_idx = {1; 2; 3:4; 5:6; 7:8; 9:10; 11:12; 13:14; 15:16; 17:18; 19:20; 21:22; 23:28; 29; 30;...
@@ -57,7 +69,14 @@ param_idx = {1; 2; 3:4; 5:6; 7:8; 9:10; 11:12; 13:14; 15:16; 17:18; 19:20; 21:22
 data = struct;
 for ii = 1:length(param_lbls)
     param = param_lbls{ii};
-    if ~isempty(strfind(param,'.')), param = extractAfter(param,'.'); end
+    if ~isempty(strfind(param,'.')) 
+        if strcmpi(version('-release'),'2016b')
+            param = extractAfter(param,'.'); 
+        else
+            ix = strfind(param,'.');
+            param = param(ix(end)+1:end);
+        end
+    end
     
     eval(['data.' char(param) ' = dataArray(:,param_idx{ii});']);
 end
