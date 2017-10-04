44992101f56e1e33d9e8234509675786b2693c0a
niek <niekbeckers@gmail.com>
2017-10-04 12:54:11 +0200

    removed white space

--- experiments/motorlearning/protocol-xml-mat/createBROSExperimentProtocol_LearningExp.m
+++ experiments/motorlearning/protocol-xml-mat/createBROSExperimentProtocol_LearningExp.m
@@ -116,17 +116,11 @@ if strcmpi(groupType,'solo')
     connectionStiffness = zeros(numTrials,1);
     connectionDamping = zeros(numTrials,1);
 elseif strcmpi(groupType,'interaction')
-    
-    
-    
     connected = zeros(21,1); connected(2:2:end) = 1;
     connected = repmat(connected,4,1);
 
 %     connected1 = zeros(42,1); connected1(2:2:end) = 1;
 %     connected = [connected1;connected;connected];
-    
-    
-    
     connectionStiffness = connected*Ks;
     connectionDamping = connected*Ds;
 
