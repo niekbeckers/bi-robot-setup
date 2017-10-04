2916edee3087acfa17f2af681e8cafa790800123
Niek-TwinCAT <niekbeckers@gmail.com>
2017-10-04 13:12:10 +0200

    loadBROSdata: added fix for exptrialnumber when experiment was restarted.

--- tools/loadBROSdata.m
+++ tools/loadBROSdata.m
@@ -14,9 +14,29 @@ if nargin < 2
     savepath = datapath;
 end
 
+dt = 0.001;
+
 % import data
 dataraw = importTCdata(datapath);
 trialnumbers = unique(dataraw.ExpTrialNumber);
+
+% check how many sequences of trial running we have. This number should be
+% the same as the length of trialnumber. If not, probably this is due to
+% restarting the experiment after an error or something. Fix it here.
+trialrunning = findseq(double(dataraw.ExpTrialRunning));
+idxtrialrunning = find(trialrunning(:,1)==1);
+
+if length(trialnumbers) ~= length(idxtrialrunning)
+    
+   warning('Inconsistent trialnumbers and dataraw.ExpTrialNumbers. Fixing it.');
+   trialnumbers = 1:length(idxtrialrunning);
+   
+   % also fix dataraw
+   for ii = 1:length(idxtrialrunning)
+       idx = trialrunning(idxtrialrunning(ii),2):trialrunning(idxtrialrunning(ii),3);
+       dataraw.ExpTrialNumber(idx) = trialnumbers(ii);
+   end
+end
 
 % get the variable names. Remove 'BusActuator1', 'BusActuator2' and 't'
 vars = {'q_AbsEnc_BROS1','q_AbsEnc_BROS2','qdot_AbsEnc_BROS1','qdot_AbsEnc_BROS2','x_AbsEnc_BROS1','x_AbsEnc_BROS2','xdot_AbsEnc_BROS1','xdot_AbsEnc_BROS2','ForcesOpSpace_BROS1','ForcesOpSpace_BROS2','target_BROS1','target_BROS2','cursor_BROS1','cursor_BROS2'}; 
@@ -33,7 +53,7 @@ for ii = 1:length(trialnumbers)
     data.trial(ii).t = t;
     
     % retrieve dt (assume it's a multiple of 1ms)
-    dt = round(mode(diff(t))/0.001)*0.001;
+    dt = round(mode(diff(t))/dt)*dt;
 
     % select the data per trial
     for jj = 1:length(vars)
