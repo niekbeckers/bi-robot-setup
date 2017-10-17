4a5f51236b6dda73116b50f8fa8dc737cb52841f
niek <niekbeckers@gmail.com>
2017-10-17 21:00:44 +0200

    Updated importTCdata, now you can indicate which model you're using (base model, sysid model)

--- tools/importTCdata.m
+++ tools/importTCdata.m
@@ -12,17 +12,6 @@ function data = importTCdata(datapath, modelname)
 if nargin < 1
     datapath = uigetdir;
 end
-if nargin == 2
-    % model name is given, load variable names from model
-    load_system(get_param(modelname,'filename'));
-    str_param_BROS1 = strsplit(get_param([modelname '/BusSelectorLoggerBROS1'],'OutputSignals'),',').';
-    str_param_BROS1 = strcat(str_param_BROS1,'_BROS1');
-    str_param_BROS2 = strsplit(get_param([modelname '/BusSelectorLoggerBROS2'],'OutputSignals'),',').';
-    str_param_BROS2 = strcat(str_param_BROS2,'_BROS2');
-else
-    str_param_BROS1 = {'SystemState_BROS1';'qJointSpace.q_AbsEnc_BROS1';'qJointSpace.qdot_AbsEnc_BROS1';'qJointSpace.q_IncEnc_BROS1';'qJointSpace.qdot_IncEnc_BROS1';'xOpSpace.x_AbsEnc_BROS1';'xOpSpace.xdot_AbsEnc_BROS1';'xOpSpace.x_BROS1';'xOpSpace.xdot_BROS1';'Controller_ToLogger.x_ref_BROS1';'Controller_ToLogger.xdot_ref_BROS1';'Controller_ToLogger.xddot_ref_BROS1';'qJointSpace.JointTorque_Nm_BROS1';'BusFTSensor.ForcesOpSpace_BROS1'};
-    str_param_BROS2 = {'SystemState_BROS2';'qJointSpace.q_AbsEnc_BROS2';'qJointSpace.qdot_AbsEnc_BROS2';'qJointSpace.q_IncEnc_BROS2';'qJointSpace.qdot_IncEnc_BROS2';'xOpSpace.x_AbsEnc_BROS2';'xOpSpace.xdot_AbsEnc_BROS2';'xOpSpace.x_BROS1';'xOpSpace.xdot_BROS1';'Controller_ToLogger.x_ref_BROS2';'Controller_ToLogger.xdot_ref_BROS2';'Controller_ToLogger.xddot_ref_BROS2';'qJointSpace.JointTorque_Nm_BROS2';'BusFTSensor.ForcesOpSpace_BROS2'};
-end
 
 %% load measurement data
 % currentdir = [fileparts(mfilename('fullpath')) filesep];
@@ -52,40 +41,69 @@ end
 cd(currentdir); % go back to current directory
 dataArray = dataArray';       % to columns
 
-% these are the parameter labels in the recorded data
-param_lbls = ['time';
-    str_param_BROS1;
-    str_param_BROS2;
-    'target_BROS1';
-    'target_BROS2';
-    'cursor_BROS1';
-    'cursor_BROS2';
-    'Error_BROS1';
-    'Error_BROS2';
-    'ExpTrialNumber';
-    'ExpTrialRunning';
-    'Admittance_Mv';
-    'Admittance_Bv'];   
 
-% indices corresponding to each parameter
-param_idx = {1;... % time 
-             2;  3:4;   5:6;   7:8;   9:10;  11:12; 13:14; 15:16; 17:18; 19:20; 21:22; 23:24; 25:26; 27:32;... % BROS1
-             33; 34:35; 36:37; 38:39; 40:41; 42:43; 44:45; 46:47; 48:49; 50:51; 52:53; 54:55; 56:57; 58:63;... % BROS2
-             64:65; 66:67; 68:69; 70:71; 72; 73;... % target, cursor, error
-             74; 75;... % experiment trial
-             76; 77; }; % admittance
-
-% add more admittance data  ("freshly added", so append to param_idx and param_lbls)
-if (size(dataArray,2) > 77)
-    param_lbls = [param_lbls; 'isConnected'; 'Kp_s'; 'Kd_s'; 'Fs1'; 'Fs2'];
-    param_idx = [param_idx; 78; 79; 80;81:82; 83:84 ]; 
+%% parameter names/labels
+% based on which model, make parameter labels and indices
+if (nargin < 2)
+    modelname = 'model_base_bros';
 end
 
-% I also added the target velocity to the model, so append to param_idx and param_lbls
-if (size(dataArray,2) > 84)
-    param_lbls = [param_lbls; 'target_vel_BROS1'; 'target_vel_BROS2'];
-    param_idx = [param_idx; 85:86; 87:88 ]; %#ok<NASGU>
+str_param_def = {'SystemState';'qJointSpace.q_AbsEnc';'qJointSpace.qdot_AbsEnc';'qJointSpace.q_IncEnc';'qJointSpace.qdot_IncEnc';'xOpSpace.x_AbsEnc';'xOpSpace.xdot_AbsEnc';'xOpSpace.x';'xOpSpace.xdot';'Controller_ToLogger.x_ref';'Controller_ToLogger.xdot_ref';'Controller_ToLogger.xddot_ref';'qJointSpace.JointTorque_Nm';'BusFTSensor.ForcesOpSpace'};
+
+switch lower(modelname)
+   
+    case 'model_base_bros'
+        %% model_base_bros
+        param_lbls = [  'time';
+                        strcat(str_param_def,'_BROS1');
+                        strcat(str_param_def,'_BROS2');
+                        'target_BROS1';
+                        'target_BROS2';
+                        'cursor_BROS1';
+                        'cursor_BROS2';
+                        'Error_BROS1';
+                        'Error_BROS2';
+                        'ExpTrialNumber';
+                        'ExpTrialRunning';
+                        'Admittance_Mv';
+                        'Admittance_Bv'];
+                    
+        % indices corresponding to each parameter
+        param_idx = {1;... % time 
+                     2;  3:4;   5:6;   7:8;   9:10;  11:12; 13:14; 15:16; 17:18; 19:20; 21:22; 23:24; 25:26; 27:32;... % BROS1
+                     33; 34:35; 36:37; 38:39; 40:41; 42:43; 44:45; 46:47; 48:49; 50:51; 52:53; 54:55; 56:57; 58:63;... % BROS2
+                     64:65; 66:67; 68:69; 70:71; 72; 73;... % target, cursor, error
+                     74; 75;... % experiment trial
+                     76; 77; }; % admittance
+        
+        % add more admittance data  ("freshly added", so append to param_idx and param_lbls)
+        if (size(dataArray,2) > 77)
+            param_lbls = [param_lbls; 'isConnected'; 'Kp_s'; 'Kd_s'; 'Fs1'; 'Fs2'];
+            param_idx = [param_idx; 78; 79; 80;81:82; 83:84 ]; 
+        end
+
+        % I also added the target velocity to the model, so append to param_idx and param_lbls
+        if (size(dataArray,2) > 84)
+            param_lbls = [param_lbls; 'target_vel_BROS1'; 'target_vel_BROS2'];
+            param_idx = [param_idx; 85:86; 87:88 ]; %#ok<NASGU>
+        end
+        
+
+    case 'model_sysid'
+        %% model_sysid
+        param_lbls = [  'time';
+                        strcat(str_param_def,'_BROS1');
+                        'Error_BROS1';
+                        'ExpTrialNumber';
+                        'ExpTrialRunning'];
+        param_idx = {1;... % time 
+                     2;  3:4;   5:6;   7:8;   9:10;  11:12; 13:14; 15:16; 17:18; 19:20; 21:22; 23:24; 25:26; 27:32;... % BROS1
+                     33; %error
+                     34; 35;... % experiment trial
+                     };  %#ok<NASGU>        
 end
+
+ 
 
 % create struct with all data parameters
 data = struct;
@@ -102,3 +120,30 @@ for ii = 1:length(param_lbls)
     
     eval(['data.' char(param) ' = dataArray(:,param_idx{ii});']);
 end
+
+
+
+%  str_param_BROS1 = {'SystemState_BROS1';'qJointSpace.q_AbsEnc_BROS1';'qJointSpace.qdot_AbsEnc_BROS1';'qJointSpace.q_IncEnc_BROS1';'qJointSpace.qdot_IncEnc_BROS1';'xOpSpace.x_AbsEnc_BROS1';'xOpSpace.xdot_AbsEnc_BROS1';'xOpSpace.x_BROS1';'xOpSpace.xdot_BROS1';'Controller_ToLogger.x_ref_BROS1';'Controller_ToLogger.xdot_ref_BROS1';'Controller_ToLogger.xddot_ref_BROS1';'qJointSpace.JointTorque_Nm_BROS1';'BusFTSensor.ForcesOpSpace_BROS1'};
+%  str_param_BROS2 = {'SystemState_BROS2';'qJointSpace.q_AbsEnc_BROS2';'qJointSpace.qdot_AbsEnc_BROS2';'qJointSpace.q_IncEnc_BROS2';'qJointSpace.qdot_IncEnc_BROS2';'xOpSpace.x_AbsEnc_BROS2';'xOpSpace.xdot_AbsEnc_BROS2';'xOpSpace.x_BROS1';'xOpSpace.xdot_BROS1';'Controller_ToLogger.x_ref_BROS2';'Controller_ToLogger.xdot_ref_BROS2';'Controller_ToLogger.xddot_ref_BROS2';'qJointSpace.JointTorque_Nm_BROS2';'BusFTSensor.ForcesOpSpace_BROS2'};
+
+%     % model name is given, load variable names from model
+%     load_system(get_param(modelname,'filename'));
+%     str_param_BROS1 = strsplit(get_param([modelname '/BusSelectorLoggerBROS1'],'OutputSignals'),',').';
+%     str_param_BROS1 = strcat(str_param_BROS1,'_BROS1');
+%     str_param_BROS2 = strsplit(get_param([modelname '/BusSelectorLoggerBROS2'],'OutputSignals'),',').';
+%     str_param_BROS2 = strcat(str_param_BROS2,'_BROS2');
+
+% these are the parameter labels in the recorded data
+% param_lbls = ['time';
+%     str_param_BROS1;
+%     str_param_BROS2;
+%     'target_BROS1';
+%     'target_BROS2';
+%     'cursor_BROS1';
+%     'cursor_BROS2';
+%     'Error_BROS1';
+%     'Error_BROS2';
+%     'ExpTrialNumber';
+%     'ExpTrialRunning';
+%     'Admittance_Mv';
+%     'Admittance_Bv'];   
