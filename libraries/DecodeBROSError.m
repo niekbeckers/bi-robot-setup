62f31ae016cc6fa215695df8c9b4f9e2b7ba7822
Niek-TwinCAT <niekbeckers@gmail.com>
2017-11-03 23:29:25 +0100

    Added read and write functions for BROSErrorDescription file (to be used by C++)

--- libraries/DecodeBROSError.m
+++ libraries/DecodeBROSError.m
@@ -31,30 +31,14 @@ else
 end;
 
 %% Decode the value
-errorSingleActuator = {...
-    'Joint position guard',...
-    'Joint velocity guard',...
-    'Joint torque guard',...
-    'Transmission slip',...
-    'Drive error',...
-    'Emergency button pressed', ...
-    'Motor undervoltage', ...
-    'Motor overvoltage', ...
-    'Drive overtemperature', ...
-    'I2t error', ...
-    'Overcurrent', ...
-    'Following/control error', ...
-    'Short circuit error',...
-    'Other drive error (see MotionErrorRegister)'
-    };
-errorDescriptions = [...
-    strcat('Act1: ',errorSingleActuator);
-    strcat('Act2: ',errorSingleActuator);
-    'Endpoint position guard',...
-    'High condition number (singular)',...
-    'Internal angle guard',...
-    'F/T sensor range exceeded'
-   ];
+
+if exist('BROSErrorDescriptions.txt','file')
+    errorDescriptions = readErrorMsgFile('BROSErrorDescriptions.txt');
+else
+    % error description file does not exist, write error description file
+    errorDescriptions = writeErrorMsgFile('BROSErrorDescriptions.txt');
+end
+
 s='';
 for i=1:length(valuesToDecode)
     v = valuesToDecode(i);
@@ -72,14 +56,16 @@ for i=1:length(valuesToDecode)
         vv = int32(abs(v));
         n = int32(2^(j-1));
         if bitand(vv,n)
-            s=strvcat(s,sprintf('   %s %s', hex(n), errorDescriptions{j}));
+%             s=strvcat(s,sprintf('   %s %s', hex(n), errorDescriptions{j}));
+            s = char(s,sprintf('   %i %s', j, errorDescriptions{j}));
             anyError = 1;
         end;
     end;
     
-    s=strvcat(s,'');
+    s=char(s,'');
     
-end;
+end
+end
 
 
 function s=hex(x)
@@ -89,3 +75,71 @@ if x<0
     x=2^32+x;
 end;
 s=['0x' dec2hex(x,8)];
+end
+
+function emsgs = writeErrorMsgFile(filename)
+% error messages
+errorSingleActuator = {...
+    'Joint position guard';...
+    'Joint velocity guard';...
+    'Joint torque guard';...
+    'Transmission slip';...
+    'Drive error';...
+    'Emergency button pressed'; ...
+    'Motor undervoltage'; ...
+    'Motor overvoltage'; ...
+    'Drive overtemperature'; ...
+    'I2t error'; ...
+    'Overcurrent'; ...
+    'Following/control error'; ...
+    'Short circuit error';...
+    'Other drive error (see MotionErrorRegister)'
+    };
+emsgs = [...
+    strcat('Act1.',errorSingleActuator);...
+    strcat('Act2.',errorSingleActuator);...
+    'Endpoint position guard';...
+    'High condition number (singular)';...
+    'Internal angle guard';...
+    'F/T sensor range exceeded'
+   ];
+
+% write error messages to file (so we use the same in C++)
+fid = fopen(filename,'w');
+if fid > 0
+    for ii = 1:length(emsgs)
+        fprintf(fid,'%s',emsgs{ii});
+        if ii < length(emsgs)
+            fprintf(fid,'\n');
+        end
+    end
+    fclose(fid);
+end
+
+end
+
+function emsgs = readErrorMsgFile(filename)   
+% Read columns of data as text:
+% For more information, see the TEXTSCAN documentation.
+formatSpec = '%s%[^\n\r]';
+
+% Open the text file.
+fileID = fopen(filename,'r');
+
+% Read columns of data according to the format.
+% This call is based on the structure of the file used to generate this
+% code. If an error occurs for a different file, try regenerating the code
+% from the Import Tool.
+dataArray = textscan(fileID, formatSpec, 'Delimiter', '', 'MultipleDelimsAsOne', true,  'ReturnOnError', false);
+
+fclose(fileID);
+
+% Convert the contents of columns containing numeric text to numbers.
+% Replace non-numeric text with NaN.
+raw = repmat({''},length(dataArray{1}),length(dataArray)-1);
+for col=1:length(dataArray)-1
+    raw(1:length(dataArray{col}),col) = dataArray{col};
+end
+
+emsgs = raw(:,1);
+end
