3ba4dbee6925fe56575153a5a5863c78ae615ece
niek <niekbeckers@gmail.com>
2017-11-09 14:47:22 +0100

    added model_virtualpartner_bros for Jolein. Need to test still.

--- tools/importTCdata.m
+++ tools/importTCdata.m
@@ -86,6 +86,41 @@ switch lower(modelname)
             param_idx = [param_idx; 85:86; 87:88 ]; 
         end
         
+    case 'model_virtualpartner_bros'
+        %% model_base_bros
+        param_lbls = [  'time';
+                        strcat(str_param_def,'_BROS1');
+                        strcat(str_param_def,'_BROS2');
+                        'target_BROS1';
+                        'target_BROS2';
+                        'cursor_BROS1';
+                        'cursor_BROS2';
+                        'Error_BROS1';
+                        'Error_BROS2';
+                        'ExpTrialNumber';
+                        'ExpTrialRunning';
+                        'Admittance_Mv';
+                        'Admittance_Bv'];
+                    
+        % indices corresponding to each parameter
+        param_idx = {1;... % time 
+                     2;  3:4;   5:6;   7:8;   9:10;  11:12; 13:14; 15:16; 17:18; 19:20; 21:22; 23:24; 25:26; 27:32;... % BROS1
+                     33; 34:35; 36:37; 38:39; 40:41; 42:43; 44:45; 46:47; 48:49; 50:51; 52:53; 54:55; 56:57; 58:63;... % BROS2
+                     64:65; 66:67; 68:69; 70:71; 72; 73;... % target, cursor, error
+                     74; 75;... % experiment trial
+                     76; 77; }; % admittance
+        
+        % add more admittance data  ("freshly added", so append to param_idx and param_lbls)
+        if (size(dataArray,2) > 77)
+            param_lbls = [param_lbls; 'isConnected'; 'Kp_s'; 'Kd_s'; 'Fs1'; 'Fs2'];
+            param_idx = [param_idx; 78; 79; 80;81:82; 83:84 ]; 
+        end
+
+        % I also added the target velocity to the model, so append to param_idx and param_lbls
+        if (size(dataArray,2) > 84)
+            param_lbls = [param_lbls; 'pos_vp1','vel_vp1','y_vp1','xhatp_vp1','pos_vp2','vel_vp2','target_vel_BROS1'; 'target_vel_BROS2'];
+            param_idx = [param_idx; 85:86; 87:88; 89:96; 97:110; 111:112; 113:114; 115:116; 117:118]; 
+        end
 
     case 'model_sysid'
         
