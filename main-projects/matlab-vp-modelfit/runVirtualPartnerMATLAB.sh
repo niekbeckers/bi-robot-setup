878a9bb4f822b562ec903d6222523af46fffe57e
niek <niekbeckers@gmail.com>
2018-01-10 14:07:50 +0100

    added shell script for remote start of matlabVirtualPartner.m

---
+++ main-projects/matlab-vp-modelfit/runVirtualPartnerMATLAB.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/niek/MATLAB/2017b/bin/matlab -nodisplay -nosplash -r "try, run('/home/niek/repositories/bros_experiments/main-projects/matlab-vp-modelfit/matlabVirtualPartner.m'), catch, quit, end, quit" ; /bin/bash
