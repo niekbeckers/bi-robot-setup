e3833e87cbf329a8556736b7f90007a51278a299
Niek-TwinCAT <niekbeckers@gmail.com>
2018-02-21 11:19:31 +0100

    updated protocol generator for virtual partner

--- experiments/virtual-agent/exp-protocol/create_experimentprotocol_virtualpartner.m
+++ experiments/virtual-agent/exp-protocol/create_experimentprotocol_virtualpartner.m
@@ -1,85 +1,15 @@
 %% createBROSExperimentProtocol_template
 %
-% - Niek
-%
-%
-% For this to work, you need to download struct2xml (https://nl.mathworks.com/matlabcentral/fileexchange/28639-struct2xml)
-% The BROS GUI and experiment handler software reads a XML file with the
-% following structure:
-%
-% <?xml version="1.0" encoding="utf-8"?>
-% <experiment>
-%    <countDownDuration>3</countDownDuration> 
-%    <trialFeedback>0</trialFeedback>
-%     <trialPerformanceThreshold>0.1</trialPerformanceThreshold>
-%     or:
-%     <trialMTRangeLower>0.8</trialMTRangeLower>
-%     <trialMTRangeUpper>1.2</trialMTRangeUpper>
-%    <block>
-%       <breakDuration>300</breakDuration>
-%       <homingType>302</homingType>
-%       <trial>
-%          <connected>1</connected>
-%          <connectionStiffness>60</connectionStiffness>
-%          <condition>1</condition>
-%          <trialDuration>40</trialDuration>
-%          <breakDuration>6</breakDuration>
-%          <trialRandomization>1</trialRandomization>
-%       </trial>
-%       <trial>
-%          ...
-%       </trial>
-%    </block>
-%    <block>
-%       <breakDuration>300</breakDuration>
-%       <homingType>302</homingType>
-%       <trial>
-%          ...
-%       </trial>
-%    </block>
-% </experiment>
-%
-% Most/some of the parameters per trial are sent to the simulink model
-% through the TwinCAT ADS server.
-% The names of the parameters have to match the following:
-%
-% - Parameters
-%   - Experiment:
-%       countDownDuration:      duration of countdown [s] (default: 3s)
-%       trialFeedback:          trial performance feedback to user [-]
-%                               0: no feedback
-%                               1: Mean Squared Error
-%                               2: Movement time
-%                               Based on the performance trial feedback
-%                               type, you can set lower, upper bounds to
-%                               movement time or a MSE performance
-%                               threshold. 
-%       trialPerformanceThreshold: MSE threshold whether performance is
-%                               better or worse than previous performance.
-%       trialMTRangeLower       Movement time lower bound
-%       trialMTRangeUpper       Movement time upper bound
-%   - Trial: 
-%       connected:              physical connection between BROS1 and BROS2 (bool)
-%       connectionStiffness:    connection stiffness [N/m] (double)
-%       connectionDamping:      connection damping [Ns/m] (double)
-%       condition:              condition number, general use (int)
-%       trialDuration:          optional (double)
-%       breakDuration:          break duration (in between trial) [s] (double)
-%       trialRandomization:     integer to select the phase sets e.g. (int)
-%   - Block:
-%       breakDuration:          optional break duration between blocks [s] (double)
-%       homingType:             301: manual homing (go to home position), 302: automatic
-%
 % Niek Beckers
 % May 2017
 
 clear all; close all; clc;
 
-partnersNr = 101;
+partnersNr = 111;
 sessionnr = 1;
 selectPremadeTrialSequence = 1;
-groupType = 'solo'; % solo or interaction
-groupTypeNr = 0; % 0 = solo, 1 = interaction
+groupType = 'vp_expert'; % solo or interaction
+groupTypeNr = 3; % 0 = solo, 1 = interaction, 2 = vp, 3 = vp_expert
 Ks = 150;
 Ds = 2;
 expID = ['virtualpartner_partners' num2str(partnersNr) '_session' num2str(sessionnr) '_type' num2str(groupTypeNr)];
@@ -114,43 +44,20 @@ s.experiment.activeBROSID.id1 = 2;
 % trial settings
 
 % experiment settings
-switch sessionnr
-    case 1
-        condition = zeros(5,1);
-%         condition = [zeros(21,1); ones(21,1); ones(21,1); zeros(21,1)];
-    case 2
-        condition = [zeros(21,1); ones(21,1); ones(21,1)];
-end
+condition = [zeros(21,1); ones(21,1); ones(21,1)];
     
 numTrials = numel(condition); % example
 breakDuration = 10*ones(numTrials,1);
 trialDuration = 20*ones(numTrials,1);
 
 % connection
-if strcmpi(groupType,'solo')
-    connected = zeros(numTrials,1);
-    connectionStiffness = zeros(numTrials,1);
-    connectionDamping = zeros(numTrials,1);
-elseif strcmpi(groupType,'interaction')
-    connected = zeros(21,1); connected(2:2:end) = 1;
-    connected = repmat(connected,4,1);
-
-%     connected1 = zeros(42,1); connected1(2:2:end) = 1;
-%     connected = [connected1;connected;connected];
-    connectionStiffness = connected*Ks;
-    connectionDamping = connected*Ds;
-
-end
-
+connected = zeros(21,1); connected(2:2:end) = 1;
+connected = repmat(connected,3,1);
+connectionStiffness = connected*Ks;
+connectionDamping = connected*Ds;
+    
 % specify how the trials are divided over the blocks
-switch sessionnr
-    case 1
-        divTrials = {1:5};
-%         divTrials = {1:21 22:42 43:63 64:84};
-    case 2
-        divTrials = {1:21 22:42 43:63};
-end
-
+divTrials = {1:21 22:42 43:63};
 
 %% randomization
 % sort elements of trialRandomization in random order
@@ -198,7 +105,7 @@ for ii = 1:numTrials
     trial{ii}.trialRandomization = trialRandomization(ii);
     
     % only fit single trials
-    if ~connected(ii) 
+    if ~connected(ii) && ~usePresetParamsVP
         trial{ii}.fitVirtualPartner.id0 = 1;
 %         trial{ii}.fitVirtualPartner.id1 = 2;
     end
