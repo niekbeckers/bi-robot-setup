a03da266347c2cd40a5ec36f9fb0393227da424c
TestActuatorSetup <no@email.nl>
2018-05-30 23:31:02 +0200

    experiment protocol for pilot tests.

---
+++ experiments/id/protocol/exp3_create_experimentprotocol_id.m
@@ -0,0 +1,125 @@
+%% createBROSExperimentProtocol_template
+% Niek Beckers
+% May 2017
+
+clear all; close all; clc;
+
+% NOTE: pair 6 was collected using the wrong model (TC), excluded. 
+
+pairNr = 99;
+groupType = 'solo'; % solo or interaction
+groupTypeNr = 0; % 0 = solo, 1 = interaction
+Ks = 150;
+Ds = 2;
+expID = ['id_pair' num2str(pairNr) '_type' num2str(groupTypeNr)];
+
+% filename
+protocolpath = 'exp3_protocols';
+filename = ['protocol_00fd_' expID];
+
+% create (main) struct
+s = struct;
+
+% indicate which type of trial feedback
+s.experiment.expID = expID;
+s.experiment.trialFeedback = 1;
+s.experiment.trialPerformanceThreshold = 0.05;
+s.experiment.groupTypeNr = groupTypeNr;
+s.experiment.sessionNr = 1;
+s.experiment.partnersNr = pairNr;
+
+% s.experiment.doVirtualPartner = 0;
+s.experiment.activeBROSID.id0 = 1;
+s.experiment.activeBROSID.id1 = 2;
+
+%% trial data
+
+% trial settings
+
+% experiment settings
+condition = [3*ones(10,1)];
+    
+numTrials = numel(condition); % example
+breakDuration = 10*ones(numTrials,1);
+trialDuration = 81.92*ones(numTrials,1);
+
+% connection
+if strcmpi(groupType,'solo')
+    connected = zeros(numTrials,1);
+    connectionStiffness = zeros(numTrials,1);
+    connectionDamping = zeros(numTrials,1);
+elseif strcmpi(groupType,'interaction')
+    connected = zeros(21,1); connected(2:2:end) = 1;
+    connected = repmat(connected,4,1);
+
+%     connected1 = zeros(42,1); connected1(2:2:end) = 1;
+%     connected = [connected1;connected;connected];
+    connectionStiffness = connected*Ks;
+    connectionDamping = connected*Ds;
+
+end
+
+% specify how the trials are divided over the blocks
+divTrials = {1:10};
+
+
+%% randomization
+trialRandomization = zeros(numTrials,1);
+
+%% prepare trials
+for ii = 1:numTrials
+    trial{ii}.connected = connected(ii);
+    trial{ii}.connectionStiffness = connectionStiffness(ii);
+    trial{ii}.connectionDamping = connectionDamping(ii);
+    trial{ii}.condition = condition(ii);
+    trial{ii}.trialDuration = trialDuration(ii);
+    trial{ii}.breakDuration = breakDuration(ii);
+    trial{ii}.trialRandomization = trialRandomization(ii);
+end
+
+%% block data
+% indicate how the trials are divided over the blocks
+% NOTE: you always need at least 1 block
+numBlocks = length(divTrials);
+for ii = 1:numBlocks
+    s.experiment.block{ii}.breakDuration = 240.0;
+    s.experiment.block{ii}.homingType = 302;
+    for jj = 1:length(divTrials{ii})
+        s.experiment.block{ii}.trial{jj} = trial{divTrials{ii}(jj)};
+    end
+    
+    expprotocol.block(ii).connected = connected(divTrials{ii});
+    expprotocol.block(ii).condition = condition(divTrials{ii});
+    expprotocol.block(ii).trialRandomization = trialRandomization(divTrials{ii});
+    expprotocol.block(ii).connectionStiffness = connectionStiffness(divTrials{ii});
+    expprotocol.block(ii).connectionDamping = connectionDamping(divTrials{ii});
+end
+
+
+
+%% save everything (in xml and mat)
+
+
+
+if ~exist(protocolpath,'dir')
+    mkdir(protocolpath);
+end
+
+% check if you want to overwrite the protocol
+saveProtocol = 1;
+if exist([protocolpath filesep filename '.xml'],'file') || exist([protocolpath filesep filename],'file')
+    promptMessage = sprintf('This file already exists:\n%s\nDo you want to overwrite it?', [protocolpath filesep filename '.xml']);
+	titleBarCaption = 'Overwrite protocol?';
+	buttonText = questdlg(promptMessage, titleBarCaption, 'Yes', 'No', 'No');
+    
+    if strcmpi(buttonText,'No')
+        saveProtocol = 0;
+    end
+end
+
+if saveProtocol
+    % write to to XML file
+    struct2xml(s,[protocolpath filesep filename '.xml']);
+    % save experiment struct in mat file
+    save([protocolpath filesep filename], 's','expprotocol');
+end
