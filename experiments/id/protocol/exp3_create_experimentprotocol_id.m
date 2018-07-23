bc329fd4d2641c04f28c8a0a07138422b528d2c6
Niek <niekbeckers@utwente.nl>
2018-07-23 09:47:56 +0200

    redid the protocols for pilot 8

--- experiments/id/protocol/exp3_create_experimentprotocol_id.m
+++ experiments/id/protocol/exp3_create_experimentprotocol_id.m
@@ -4,8 +4,6 @@
 
 clear all; close all; clc;
 
-% NOTE: pair 6 was collected using the wrong model (TC), excluded. 
-
 pairNr = 104;
 groupType = 'interaction'; % solo or interaction
 groupTypeNr = 1; % 0 = solo, 1 = interaction
