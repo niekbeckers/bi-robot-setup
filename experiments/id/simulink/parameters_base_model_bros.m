9de23f3f8ffcafc5dd86465ec544d145a7520594
Niek <niekbeckers@utwente.nl>
2018-08-08 16:43:25 +0200

    added new forcing function (target), now larger variance and more frequencies (to try out).

--- experiments/id/simulink/parameters_base_model_bros.m
+++ experiments/id/simulink/parameters_base_model_bros.m
@@ -11,7 +11,11 @@ Lb = 0.07;                      % base width [m]
 
 % load target signal data
 % load('data_target_signal.mat','nx','ny','Ax','Ay','phx','phy');
-load('forcingfunction_data_id_2d_var6_1.mat');
+% load('forcingfunction_data_id_2d_var6_1.mat');
+load('forcingfunction_data_id_2d_var75_nt10.mat');
+% load('forcingfunction_data_id_2d_var75_nt11.mat');
+% load('forcingfunction_data_id_2d_var75_nt12.mat');
+
 
 % force field
 FFMatrix = -[0 0; 0 0]; % added minus due to coordinate system flip (y pointing up)
