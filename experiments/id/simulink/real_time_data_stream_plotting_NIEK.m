54895dcb7cca5a7fae215e167997d233016aed41
Niek <niekbeckers@utwente.nl>
2018-12-07 22:50:51 +0100

    Muscle name on it

--- experiments/id/simulink/real_time_data_stream_plotting_NIEK.m
+++ experiments/id/simulink/real_time_data_stream_plotting_NIEK.m
@@ -34,7 +34,7 @@ HOST_IP = '130.89.64.30';
 %% NIEK's CUSTOM SENSOR PLOT
 global partner1sensors;
 partner1sensors = [9 1 2 3 4 10 11];
-
+str_muscles = {'pec / ant delt','post delt','biceps','tric long','tric lat','brachio rad','flexor carpi'};
 global partner2sensors;
 partner2sensors = [12 5 6 7 8 13 14];
 
@@ -120,73 +120,7 @@ for i = 1:NUM_SENSORS
 end
 
 
-% PARTNER 1 figure
-figureHandleEMG_BROS1 = figure('Name', 'EMG BROS1','Numbertitle', 'off',  'CloseRequestFcn', {@localCloseFigure, interfaceObjectEMG, interfaceObjectACC, commObject, t});
-set(figureHandleEMG_BROS1, 'position', [50 500 750 750])
 
-for i = 1:7
-    axesHandlesEMG_BROS1(i) = subplot(2,4,i);
-
-    plotHandlesEMG_BROS1(i) = plot(axesHandlesEMG_BROS1(i),0,'-y','LineWidth',1);
-
-    set(axesHandlesEMG_BROS1(i),'YGrid','on');
-    %set(axesHandlesEMG(i),'YColor',[0.9725 0.9725 0.9725]);
-    set(axesHandlesEMG_BROS1(i),'XGrid','on');
-    %set(axesHandlesEMG(i),'XColor',[0.9725 0.9725 0.9725]);
-    set(axesHandlesEMG_BROS1(i),'Color',[.15 .15 .15]);
-    set(axesHandlesEMG_BROS1(i),'YLim', [-.005 .005]*0.4);
-    set(axesHandlesEMG_BROS1(i),'YLimMode', 'manual');
-    set(axesHandlesEMG_BROS1(i),'XLim', [0 2000]);
-    set(axesHandlesEMG_BROS1(i),'XLimMode', 'manual');
-    
-    if(mod(i, 4) == 1)
-        ylabel(axesHandlesEMG_BROS1(i),'V');
-    else
-        set(axesHandlesEMG_BROS1(i), 'YTickLabel', '')
-    end
-    
-    if(i >12)
-        xlabel(axesHandlesEMG_BROS1(i),'Samples');
-    else
-        set(axesHandlesEMG_BROS1(i), 'XTickLabel', '')
-    end
-    
-    title(sprintf('EMG %i', partner1sensors(i))) 
-end
-
-% PARTNER 2 figure
-figureHandleEMG_BROS2 = figure('Name', 'EMG BROS2','Numbertitle', 'off',  'CloseRequestFcn', {@localCloseFigure, interfaceObjectEMG, interfaceObjectACC, commObject, t});
-set(figureHandleEMG_BROS2, 'position', [850 500 750 750])
-
-for i = 1:7
-    axesHandlesEMG_BROS2(i) = subplot(2,4,i);
-
-    plotHandlesEMG_BROS2(i) = plot(axesHandlesEMG_BROS2(i),0,'-y','LineWidth',1);
-
-    set(axesHandlesEMG_BROS2(i),'YGrid','on');
-    %set(axesHandlesEMG(i),'YColor',[0.9725 0.9725 0.9725]);
-    set(axesHandlesEMG_BROS2(i),'XGrid','on');
-    %set(axesHandlesEMG(i),'XColor',[0.9725 0.9725 0.9725]);
-    set(axesHandlesEMG_BROS2(i),'Color',[.15 .15 .15]);
-    set(axesHandlesEMG_BROS2(i),'YLim', [-.005 .005]*0.4);
-    set(axesHandlesEMG_BROS2(i),'YLimMode', 'manual');
-    set(axesHandlesEMG_BROS2(i),'XLim', [0 2000]);
-    set(axesHandlesEMG_BROS2(i),'XLimMode', 'manual');
-    
-    if(mod(i, 4) == 1)
-        ylabel(axesHandlesEMG_BROS2(i),'V');
-    else
-        set(axesHandlesEMG_BROS2(i), 'YTickLabel', '')
-    end
-    
-    if(i >12)
-        xlabel(axesHandlesEMG_BROS2(i),'Samples');
-    else
-        set(axesHandlesEMG_BROS2(i), 'XTickLabel', '')
-    end
-    
-    title(sprintf('EMG %i', partner2sensors(i))) 
-end
 
 
 %initiate the ACC figure
@@ -228,6 +162,74 @@ for i= 1:NUM_SENSORS
     
     title(sprintf('ACC %i', i)) 
 
+end
+
+% PARTNER 1 figure
+figureHandleEMG_BROS1 = figure('Name', 'EMG BROS1','Numbertitle', 'off',  'CloseRequestFcn', {@localCloseFigure, interfaceObjectEMG, interfaceObjectACC, commObject, t});
+set(figureHandleEMG_BROS1, 'position', [50 500 1000 500])
+
+for i = 1:7
+    axesHandlesEMG_BROS1(i) = subplot(2,4,i);
+
+    plotHandlesEMG_BROS1(i) = plot(axesHandlesEMG_BROS1(i),0,'-y','LineWidth',1);
+
+    set(axesHandlesEMG_BROS1(i),'YGrid','on');
+    %set(axesHandlesEMG(i),'YColor',[0.9725 0.9725 0.9725]);
+    set(axesHandlesEMG_BROS1(i),'XGrid','on');
+    %set(axesHandlesEMG(i),'XColor',[0.9725 0.9725 0.9725]);
+    set(axesHandlesEMG_BROS1(i),'Color',[.15 .15 .15]);
+    set(axesHandlesEMG_BROS1(i),'YLim', [-.005 .005]*0.4);
+    set(axesHandlesEMG_BROS1(i),'YLimMode', 'manual');
+    set(axesHandlesEMG_BROS1(i),'XLim', [0 2000]);
+    set(axesHandlesEMG_BROS1(i),'XLimMode', 'manual');
+    
+    if(mod(i, 4) == 1)
+        ylabel(axesHandlesEMG_BROS1(i),'V');
+    else
+        set(axesHandlesEMG_BROS1(i), 'YTickLabel', '')
+    end
+    
+    if(i >12)
+        xlabel(axesHandlesEMG_BROS1(i),'Samples');
+    else
+        set(axesHandlesEMG_BROS1(i), 'XTickLabel', '')
+    end
+    
+    title(sprintf('EMG %i: %s', partner1sensors(i),str_muscles{i})) 
+end
+
+% PARTNER 2 figure
+figureHandleEMG_BROS2 = figure('Name', 'EMG BROS2','Numbertitle', 'off',  'CloseRequestFcn', {@localCloseFigure, interfaceObjectEMG, interfaceObjectACC, commObject, t});
+set(figureHandleEMG_BROS2, 'position', [1100 500 1000 500])
+
+for i = 1:7
+    axesHandlesEMG_BROS2(i) = subplot(2,4,i);
+
+    plotHandlesEMG_BROS2(i) = plot(axesHandlesEMG_BROS2(i),0,'-y','LineWidth',1);
+
+    set(axesHandlesEMG_BROS2(i),'YGrid','on');
+    %set(axesHandlesEMG(i),'YColor',[0.9725 0.9725 0.9725]);
+    set(axesHandlesEMG_BROS2(i),'XGrid','on');
+    %set(axesHandlesEMG(i),'XColor',[0.9725 0.9725 0.9725]);
+    set(axesHandlesEMG_BROS2(i),'Color',[.15 .15 .15]);
+    set(axesHandlesEMG_BROS2(i),'YLim', [-.005 .005]*0.4);
+    set(axesHandlesEMG_BROS2(i),'YLimMode', 'manual');
+    set(axesHandlesEMG_BROS2(i),'XLim', [0 2000]);
+    set(axesHandlesEMG_BROS2(i),'XLimMode', 'manual');
+    
+    if(mod(i, 4) == 1)
+        ylabel(axesHandlesEMG_BROS2(i),'V');
+    else
+        set(axesHandlesEMG_BROS2(i), 'YTickLabel', '')
+    end
+    
+    if(i >12)
+        xlabel(axesHandlesEMG_BROS2(i),'Samples');
+    else
+        set(axesHandlesEMG_BROS2(i), 'XTickLabel', '')
+    end
+    
+    title(sprintf('EMG %i: %s', partner2sensors(i),str_muscles{i})) 
 end
 
 %%Open the COM interface, determine RATE
